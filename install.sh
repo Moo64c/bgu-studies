36bd3137a1e59851d456d7312e45cdf44da83b03
Amir Arbel <amirarbel@gmail.com>
2013-10-28 18:27:33 +0200

    #3: Commit working install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo rm -rf www/
+
+bash scripts/build
+
+cd www
+
+cp sites/default/default.settings.php sites/default/settings.php
+mkdir sites/default/files
+
+drush si -y bgu --account-pass=admin --db-url=mysql://root:root@localhost/bgu --db-su=root -v
+drush mi --all --user=1
+
+sudo chmod -R 777 sites/default/files
+
